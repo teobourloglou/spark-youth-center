74da637b293bcb20f82ae757b9bf57f2d2203141
Theo2801 <theologos.bourloglou@gmail.com>
2023-10-12 01:56:44 +0300

    [feat] Install Tailwind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+        dela: ['"Dela Gothic One", sans-serif'],
+        arimo: ['"Arimo", sans-serif'],
+    },
+},
+  plugins: [],
+}
+
