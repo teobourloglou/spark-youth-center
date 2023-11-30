a8164d625dc64cb4adfc41e9313294b8deb506b6
Theo2801 <theologos.bourloglou@gmail.com>
2023-12-01 01:25:29 +0200

    [refactor] Remake events logic, implementing caching

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 const sheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRbhxTRBOlPvSUHRA17RpLy_MW33Y1lcmvMzcG2xjQqiFVuqKIN3iwKKT8zAb9jDz63bQZ7VVNyecE5/pubhtml?gid=0&single=true";
 
 /**
+ * Check whether the events are cached and if not fetch the events from a google sheet URL
+ *
+ * @param {string} url - The URL that the fetch request will be sent.
+ * @param {boolean} limited - Whether to limit the number of displayed events (default is false).
+ * @returns {void}
+ */
+const fetchOrCacheEvents = (url, limited = false) => {
+  const cachedEvents = JSON.parse(localStorage.getItem("cachedEvents")) || { data: [], timestamp: 0 };
+  const cachedEventsData = cachedEvents.data;
+  const cachedEventsTimestamp = cachedEvents.timestamp;
+
+  const cacheExpiration = 86400000;
+  const isCacheValid = cachedEventsData.length > 0 && Date.now() - cachedEventsTimestamp < cacheExpiration;
+
+  isCacheValid ? displayEvents(cachedEventsData, limited) : fetchEvents(url, limited);
+}
+
+
+
+
+
+/**
  * Fetch events from a google sheet URL and return each one in an HTML format.
  *
  * @param {string} url - The URL that the fetch request will be sent.
@@ -19,6 +41,7 @@ const fetchEvents = (url, limited = false) => {
       const currentDay = currentDate.getDate();
       const currentMonth = currentDate.getMonth() + 1;
       let numberOfEvents = 0;
+      const events = [];
 
       rows.forEach((row) => {
         if (limited && numberOfEvents > 9) {
@@ -36,20 +59,51 @@ const fetchEvents = (url, limited = false) => {
               (eventMonth === currentMonth && eventDay >= currentDay)) &&
             columns.length === 6
           ) {
-            const event = createEventHTML(columns);
-            eventContainer.appendChild(event);
+            events.push({
+              0: columns[0].textContent,
+              1: columns[1].textContent,
+              2: columns[2].textContent,
+              3: columns[3].textContent,
+              4: columns[4].textContent,
+              5: columns[5].textContent,
+            });
+            
             numberOfEvents++;
           }
         } else {
           console.error("Date format does not match.");
         }
       });
+      displayEvents(events,limited)
+      localStorage.setItem("cachedEvents", JSON.stringify({ data: events, timestamp: Date.now() }));
     })
     .catch((error) => {
       console.error("Error fetching data from Google Sheet:", error);
     });
-};
 
+  };
+
+/**
+ * Display events on the webpage, limiting the number of displayed events if specified.
+ *
+ * @param {Array} events - An array of event objects to display.
+ * @param {boolean} [limited=false] - Whether to limit the number of displayed events (default is false).
+ * @returns {void}
+ */
+const displayEvents = (events, limited = false) => {
+  let numberOfEvents = 0;
+
+  events.forEach((event) => {
+    if (limited && numberOfEvents > 9) {
+      return;
+    }
+
+    const eventHTML = createEventHTML(Object.values(event));
+    eventContainer.appendChild(eventHTML);
+    numberOfEvents++;
+  });
+}
+  
 /**
  * Create HTML for an event based on column data.
  *
@@ -64,18 +118,18 @@ function createEventHTML(columns) {
     "relative flex flex-col justify-end overflow-hidden bg-gray-900 aspect-1/2 carousel-item isolate rounded-xl group";
   event.innerHTML = `
     <div class="w-full h-auto">
-        <img src="${image.textContent}" alt="" class="inset-0 w-full aspect-1/2 object-cover">
+        <img src="${image}" alt="" class="inset-0 w-full aspect-1/2 object-cover">
     </div>
     <div class="absolute bottom-2 lg:bottom-4 z-20 flex flex-col items-center gap-5 w-full p-3 opacity-100 text-center group-hover:-translate-y-8 group-hover:opacity-0 transition-all !duration-1000">
-        <h3 class="text-3xl font-dela leading-7 text-white ">${title.textContent}</h3>
+        <h3 class="text-3xl font-dela leading-7 text-white ">${title}</h3>
         <hr class="w-[72px] border-b-[3px] border-flame">
-        <p class="text-xl text-white">${date.textContent}</p>
+        <p class="text-xl text-white">${date}</p>
     </div>
     <div class="absolute w-full h-full opacity-25 group-hover:opacity-75 bg-black transition-opacity !duration-1000"></div>
     <div class="absolute w-full h-full z-10 -top-8 flex flex-col p-3 text-white justify-start gap-8 lg:justify-around opacity-0 group-hover:opacity-100 group-hover:top-0 transition-all delay-[350ms] !duration-1000">
-        <time class="font-semibold text-sm sm:text-base  text-center">🕔 ${time.textContent}</time>
-        <p class="font-semibold text-sm sm:text-base  text-center">📍 ${location.textContent}</p>
-        <p class="font-semibold text-sm sm:text-base  text-center">⚡️ ${description.textContent}</p>
+        <time class="font-semibold text-sm sm:text-base  text-center">🕔 ${time}</time>
+        <p class="font-semibold text-sm sm:text-base  text-center">📍 ${location}</p>
+        <p class="font-semibold text-sm sm:text-base  text-center">⚡️ ${description}</p>
     </div>`;
 
   return event;
@@ -97,9 +151,9 @@ const fetchComponents = (components) => {
         if (element) {
           element.innerHTML = data;
           if (name === "allEvents") {
-            fetchEvents(sheetUrl);
+            fetchOrCacheEvents(sheetUrl);
           } else if (name === "someEvents") {
-            fetchEvents(sheetUrl, true);
+            fetchOrCacheEvents(sheetUrl, true);
           }
         }
       });
